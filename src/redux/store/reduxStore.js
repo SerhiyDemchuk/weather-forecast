4f2fb4f8cee0f6a43dd19194f236613c166397d9
SerhiyDemchuk <sergeylobster@gmail.com>
2021-01-24 22:43:03 +0200

    commit

--- src/redux/store/reduxStore.js
+++
@@ -1,8 +0,0 @@
-import {createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import { weatherReducer } from '../reducer/weatherReducer';
-
-const store = createStore(weatherReducer, applyMiddleware(thunk));
-
-window.__store__ = store;
-export default store;
